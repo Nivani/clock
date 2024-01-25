8149db93ef42779ff9de2a6b3b1f168faccdae71
Nick Van Nieuwenhuyse <nick@nvnh.io>
2024-01-25 20:19:37 +0100

    fix #3 RealClock.setTimeout() now correctly forwards arguments

---
+++ src/clock.test.js
@@ -0,0 +1,29 @@
+import { describe, test, expect, vi } from "vitest";
+import { createClock } from "./clock";
+
+describe("RealClock", () => {
+  test(".setTimeout() forwards parameters", async () => {
+    const clock = createClock();
+
+    const fn = vi.fn().mockName("callback");
+    clock.setTimeout(fn, 20, "param1", 2);
+    await waitMs(30);
+
+    expect(fn).toHaveBeenCalledWith("param1", 2);
+  });
+
+  test(".setInterval() forwards parameters", async () => {
+    const clock = createClock();
+
+    const fn = vi.fn().mockName("callback");
+    const intervalId = clock.setInterval(fn, 20, "param1", 2);
+    await waitMs(30);
+    clock.clearInterval(intervalId);
+
+    expect(fn).toHaveBeenCalledWith("param1", 2);
+  });
+});
+
+async function waitMs(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
